55f17a67b7317f36e3cb88f52b5f64efa999dff8
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-05-21 19:49:57 +0300

    study django #6

--- dj_lesson/lesson_1/urls.py
+++ dj_lesson/lesson_1/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
+
+register_converter(converters.YearConverter, 'year4')
 
 
 urlpatterns = [
     path('', views.index),
     path('lesson_1/', views.next_page),
-    path('other/', views.other_page),
+    path('other/<int:other_id>/', views.other_page),
+    path('other/<slug:other_slug>', views.other_by_slug),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.other_archive)
+    path('archive/<year4:year>/', views.other_archive)
+
 ]
--- dj_lesson/lesson_1/views.py
+++ dj_lesson/lesson_1/views.py
@@ -4,8 +4,18 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('Отображение стартовой страницы домену')
 
+
 def next_page(request):
     return HttpResponse('Отображение страницы по запросу lesson_1')
 
-def other_page(request):
-    return HttpResponse('<h1>Отображение страницы по запросу other</h1>')
+
+def other_page(request, other_id):
+    return HttpResponse(f'<h1>Отображение страницы по запросу other</h1><p>id: {other_id}</p>')
+
+
+def other_by_slug(request, other_slug):
+    return HttpResponse(f'<h1>Отображение страницы по запросу other со слагом</h1><p>id: {other_slug}</p>')
+
+
+def other_archive(request, year):
+    return HttpResponse(f'<h1>Отображение страницы по запросу other </h1><p>архив: {year}</p>')

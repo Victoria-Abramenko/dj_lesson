1845fa5f3b042b7e8e86b07fa121fda99d48c9c5
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-05-28 15:18:50 +0300

    study django #12

--- dj_lesson/lesson_1/urls.py
+++ dj_lesson/lesson_1/urls.py
@@ -8,10 +8,8 @@ register_converter(converters.YearConverter, 'year4')
 urlpatterns = [
     path('', views.index, name='home_page'),
     path('about', views.about, name='about'),
-    path('lesson_1/', views.next_page, name='lesson_page'),
-    path('other/<int:other_id>/', views.other_page, name='other_number_page'),
-    path('other/<slug:other_slug>/', views.other_by_slug, name='other_slug_page'),
-    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.other_archive)
-    path('archive/<year4:year>/', views.other_archive, name='archive_page')
-
+    path('addpage/', views.add_page, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
--- dj_lesson/lesson_1/views.py
+++ dj_lesson/lesson_1/views.py
@@ -4,7 +4,13 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
+
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'биография Анджелины Джоли', 'is_published': True},
@@ -12,11 +18,6 @@ data_db = [
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'биография Джулии Робертс', 'is_published': True}
 ]
 
-
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
 
 
 def index(request):
@@ -33,32 +34,22 @@ def about(request):
     return render(request, 'lesson_temp/about.html', {'title': 'О сайте'})
 
 
-def next_page(request):
-    return HttpResponse('Отображение страницы по запросу lesson_1')
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
-def other_page(request, other_id):
-    return HttpResponse(f'<h1>Отображение страницы по запросу other</h1><p>id: {other_id}</p>')
+def add_page(request):
+    return HttpResponse(f"Добавление статьи")
 
 
-def other_by_slug(request, other_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>Отображение страницы по запросу other со слагом</h1><p>id: {other_slug}</p>')
+def contact(request):
+    return HttpResponse(f"Страница с контактами")
 
 
-def other_archive(request, year):
-    if year > 2023:
-        # return redirect('/')
-        # return redirect('/', permanent=True)
-        # return redirect(index)
-        # return redirect('home_page')
-        # return redirect('other_slug_page','music')
-        ur = reverse('other', args=('music', ))
-        # return redirect(ur)
-        return HttpResponseRedirect(ur)
+def login(request):
+    return HttpResponse(f"Авторизация")
 
-    return HttpResponse(f'<h1>Отображение страницы по запросу other </h1><p>архив: {year}</p>')
 
 def func_page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")

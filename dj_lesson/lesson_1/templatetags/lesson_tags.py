c1703f177fd97b47e46788435c61a07bd4e6b485
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-06-20 22:02:05 +0300

    study django #35

--- dj_lesson/lesson_1/templatetags/lesson_tags.py
+++ dj_lesson/lesson_1/templatetags/lesson_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 import lesson_1.views as views
 from lesson_1.models import Category, TagPosts
 
@@ -7,10 +9,10 @@ register = template.Library()
 
 @register.inclusion_tag('lesson_temp/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 
 @register.inclusion_tag('lesson_temp/list_tags.html')
 def show_all_tags(cat_selected=0):
-    return {'tags': TagPosts.objects.all()}
+    return {'tags': TagPosts.objects.annotate(total=Count("tags")).filter(total__gt=0)}

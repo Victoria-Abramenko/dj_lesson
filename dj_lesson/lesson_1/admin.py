61a14b5bfb57458748043cff00f9d945cbedeb8a
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-06-24 22:19:38 +0300

    study django #39

--- dj_lesson/lesson_1/admin.py
+++ dj_lesson/lesson_1/admin.py
@@ -1,13 +1,32 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import LessonForDB, Category
 
 
 @admin.register(LessonForDB)
 class AppAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'is_published', 'cat', "show_info")
+    list_display_links = ('title', )
     ordering = ['time_create']
     list_editable = ('is_published', )
+    list_per_page = 5
+    actions = ['set_published', 'set_draft']
+
+    @admin.display(description="Краткое описание", ordering='content')
+    def show_info(self, obj_lesson = LessonForDB):
+        return f" Описание {len(obj_lesson.content)} символов"
+
+
+    @admin.action(description="Опубликовать статью")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=LessonForDB.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count} записей")
+
+
+    @admin.action(description="Снять статью спубликации")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=LessonForDB.Status.DRAFT)
+        self.message_user(request, f"Изменено {count} записей", messages.Warning)
+
 
 
 @admin.register(Category)

18e42675ecf49673e97bbd4011f08ce4e56b008e
Victoria-Abramenko <victoria666abramenko@gmail.com>
2025-07-12 09:33:38 +0300

    study django #56

--- dj_lesson/lesson_1/views.py
+++ dj_lesson/lesson_1/views.py
@@ -8,201 +8,64 @@ from django.views.generic import TemplateView, ListView, DetailView, FormView, C
 
 from .forms import AddPostForm, UploadFileForm
 from .models import LessonForDB, Category, TagPosts, UploadFiles
-
-menu = [
-    {'title': 'О сайте', 'url_name': 'about'},
-    {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'},
-]
+from .utils import DataMixin
 
 
-# def index(request):
-#     posts = LessonForDB.published.all().select_related("cat")
-#     data = {'title': 'Главная страница сайта',
-#             'text': '',
-#             'menu': menu,
-#             'posts': posts,
-#             'cat_selected': 0}
-#     return render(request, 'lesson_temp/index.html', context=data)
-
-
-# class HomePage(TemplateView):
-#     template_name = 'lesson_temp/index.html'
-#     extra_context = {
-#         'title': 'Главная страница сайта',
-#         'text': '',
-#         'menu': menu,
-#         'posts': LessonForDB.published.all().select_related("cat"),
-#         'cat_selected': 0
-#     }
-
-
-class HomePage(ListView):
-    # model = LessonForDB
+class HomePage(DataMixin, ListView):
     template_name = 'lesson_temp/index.html'
     context_object_name = 'posts'
-    extra_context = {
-        'title': 'Главная страница сайта',
-        'text': '',
-        'menu': menu,
-        'cat_selected': 0
-    }
-
+    title_page = 'Главная страница сайта'
+    cat_selected = 0
 
     def get_queryset(self):
         return LessonForDB.published.all().select_related("cat")
-
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['title'] = 'Главная страница сайта'
-    #     context['menu'] = menu
-    #     context['posts'] =  LessonForDB.published.all().select_related("cat")
-    #     context['cat_selected'] = int(self.request.GET.get('cat_id, 0'))
-    #     return context
-
-
-# def handle_uploaded_file(f):
-#     with open(f"uploads/{f.name}", "wb+") as destination:
-#         for chunk in f.chunks():
-#             destination.write(chunk)
 
 def about(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            # handle_uploaded_file(form.cleaned_data["file"])
             fp = UploadFiles(file=form.cleaned_data["file"])
             fp.save()
     else:
         form = UploadFileForm()
 
-    return render(request, 'lesson_temp/about.html', {'title': 'О сайте', 'menu': menu, 'form': form})
+    return render(request, 'lesson_temp/about.html', {'title': 'О сайте', 'form': form})
 
 
-#
-# def show_post(request, post_slug):
-#     post = get_object_or_404(LessonForDB, slug=post_slug)
-#     data = {
-#         'title': post.title,
-#         'menu': menu,
-#         'post': post,
-#         'cat_selected': 1,
-#     }
-#
-#     return render(request, 'lesson_temp/post.html', data)
-
-
-class ShowPost(DetailView):
-    model = LessonForDB
+class ShowPost(DataMixin, DetailView):
     template_name = 'lesson_temp/post.html'
     slug_url_kwarg = 'post_slug'
     context_object_name = 'post'
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post'].title
-        context['menu'] = menu
-        return context
-
+        return self.get_mixin_context(context, title=context['post'].title)
 
     def get_object(self, queryset=None):
         return get_object_or_404(LessonForDB.published, slug=self.kwargs[self.slug_url_kwarg])
 
 
-
-# def add_page(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # try:
-#             #     LessonForDB.objects.create(**form.cleaned_data)
-#             # except:
-#             #     form.add_error(None, "Ошибка при добавлении поста")
-#             #     return redirect("home_page")
-#             form.save()
-#             return redirect("home_page")
-#     else:
-#         form = AddPostForm()
-#
-#     data = {
-#         "menu": menu,
-#         "title": "Добавление статьи",
-#         "form": form
-#     }
-#     return render(request, 'lesson_temp/addpage.html', data)
-
-
-# class AddPage(View):
-#     def get(self, request):
-#         form = AddPostForm()
-#         data = {
-#             "menu": menu,
-#             "title": "Добавление статьи",
-#             "form": form
-#         }
-#         return render(request, 'lesson_temp/addpage.html', data)
-#
-#     def post(self, request):
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("home_page")
-#
-#         data = {
-#             "menu": menu,
-#             "title": "Добавление статьи",
-#             "form": form
-#         }
-#         return render(request, 'lesson_temp/addpage.html', data)
-
-
-
-class AddPage(CreateView):
+class AddPage(DataMixin, CreateView):
     form_class = AddPostForm
-    # model = LessonForDB
-    # fields = '__all__'
     template_name = 'lesson_temp/addpage.html'
-    # success_url = reverse_lazy('home_page')
-    extra_context = {
-        'menu': menu,
-        'title': 'Добавление статьи',
-    }
+    title_page = 'Добавление статьи'
 
 
-class UpdatePage(UpdateView):
+class UpdatePage(DataMixin, UpdateView):
     model = LessonForDB
     fields = ['title', 'content', 'photo', 'is_published', 'cat']
     template_name = 'lesson_temp/addpage.html'
     success_url = reverse_lazy('home_page')
-    extra_context = {
-        'menu': menu,
-        'title': 'Изменение статьи',
-    }
-
+    title_page = 'Добавление статьи'
 
 def contact(request):
     return HttpResponse(f"Страница с контактами")
-
 
 def login(request):
     return HttpResponse(f"Авторизация")
 
 
-# def show_category(request, cat_slug):
-#     category = get_object_or_404(Category, slug=cat_slug)
-#     posts = LessonForDB.published.filter(cat_id=category.pk).select_related("cat")
-#     data = {'title': f'Рубрика: {category.name}',
-#             'text': '',
-#             'menu': menu,
-#             'posts': posts,
-#             'cat_selected': category.pk}
-#     return render(request, 'lesson_temp/index.html', context=data)
-
-
-class ShowCategory(ListView):
+class ShowCategory(DataMixin, ListView):
     template_name = 'lesson_temp/index.html'
     context_object_name = 'posts'
     allow_empty = False
@@ -215,28 +78,10 @@ class ShowCategory(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         cat = context['posts'][0].cat
-        context['title'] = 'Категория - ' + cat.name
-        context['menu'] = menu
-        context['cat_selected'] = cat.pk
-        return context
+        return self.get_mixin_context(context, title='Категория - ' + cat.name, cat_selected=cat.pk)
 
 
-
-# def show_tag_posts_list(request, tag_slug):
-#     tag = get_object_or_404(TagPosts, slug=tag_slug)
-#     posts = tag.tags.filter(is_published=LessonForDB.Status.PUBLISHED).select_related("cat")
-#
-#     data = {
-#         'title': f"Тег: {tag.tag}",
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': None,
-#     }
-#
-#     return render(request, 'lesson_temp/index.html', context=data)
-
-
-class TagPostList(ListView):
+class TagPostList(DataMixin, ListView):
     template_name = 'lesson_temp/index.html'
     context_object_name = 'posts'
     allow_empty = False
@@ -245,10 +90,7 @@ class TagPostList(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         tag = TagPosts.objects.get(slug=self.kwargs['tag_slug'])
-        context['title'] = 'Тег: ' + tag.tag
-        context['menu'] = menu
-        context['cat_selected'] = None
-        return context
+        return self.get_mixin_context(context, title='Тег: ' + tag.tag)
 
     def get_queryset(self):
         return LessonForDB.published.filter(tags__slug=self.kwargs['tag_slug']).select_related("cat")
